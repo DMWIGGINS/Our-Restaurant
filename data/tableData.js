34e0dbb6c12a35d5144dfaf7cfc3d3e44ecd3213
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2017-12-14 20:48:02 -0500

    API Table routes and posting JSON.

---
+++ data/tableData.js
@@ -0,0 +1,33 @@
+var tableData = [{
+        customerName: "benny",
+        phoneNumber: "494949``",
+        customerEmail: "k",
+        customerID: "23"
+    },
+    {
+        customerName: "zach",
+        phoneNumber: "9494949494949",
+        customerEmail: "8484848484@gmail.com",
+        customerID: "zack"
+    },
+    {
+        customerName: "Bill Clinton",
+        phoneNumber: "555-555-5569",
+        customerEmail: "bill@monica.com",
+        customerID: "I did not have ....."
+    },
+    {
+        customerName: "sfadg",
+        phoneNumber: "2322222222",
+        customerEmail: "fadgf@fgdf.com",
+        customerID: "sadkfhe"
+    },
+    {
+        customerName: "type",
+        phoneNumber: "type",
+        customerEmail: "type",
+        customerID: "type"
+    }
+]
+
+module.exports = tableData;
--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require('./routes/htmlRoutes.js')(app)
+require('./routes/htmlRoutes.js')(app);
+require('./routes/apiRoutes.js')(app);
 
 app.listen(PORT, function() {
     console.log("This is working! On " + PORT);
